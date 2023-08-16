974d3a26a79d023d5cdcb4e661a08aa4184c4b1c
Tommaso Benacchio <tbo@dmi.dk>
2023-08-16 20:10:24 +0000

    Removing .py file, adding correct .md file.

--- AQgrib2/AQ_params.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import openpyxl
-
-df=pd.read_excel('parameters_AQ_runs.xlsx')
-df = df.apply(lambda x: x.str.replace('\n', '<br>') if x.dtype == 'object' else x)
-md_table = df.to_markdown(index=False)
-print(md_table)
-
